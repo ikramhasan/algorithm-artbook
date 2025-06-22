6686357465dadcb044a539914c967755dfd70cfd
Ikram Hasan <ikramhasan.dev@gmail.com>
2025-06-23 01:53:40 +0600

    add: redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/cellular-automata/game-of-life");
+  }, [router]);
+
   return (
     <div>
       <h1>Hello World</h1>
